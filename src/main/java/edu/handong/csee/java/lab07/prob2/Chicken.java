850efaa24241744469f342db186abd6a0d4b1774
21700383 <21700383@handong.edu>
2018-04-04 20:17:21 +0900

    source code files of problem2. In process of understanding and putting java doc and comments

--- src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -1,46 +1,66 @@
 package edu.handong.csee.java.lab07.prob2;
 
+/**
+ * Chicken class having get and set method to put the user input into menu.
+ * Name, price, rating(star) is received from the user input in main
+ * and set menu's name,price and rating.
+ * Get methods are used when printing the menu's values.
+ * @author Admin
+ *
+ */
 public class Chicken {
-private String name;
-private double price;
-private int stars;
+	private String name; // declare name as private string only used in this class
+	private double price; // declare price as private double only used in this class
+	private int stars; // declare stars as private int and stars will be otherwise called rating
+	/**
+	 * Default constructor method which would run if nothing is input, setting everything to 0.
+	 */
+	public Chicken()
+	{ // declare Chicken method
+		this.name = ""; // set the name of the called menu to nothing
+		this.price = 0.0; // set the price of the called menu to 0
+		this.stars = 0; // set the stars of the called menu to 0
+	}
+	/**
+	 * Constructor method which gets the user input and set them to the called menu's name, price and starts
+	 * @param name
+	 * @param price
+	 * @param stars
+	 */
+	public Chicken(String name, double price, int stars)
+	{  // declare 
+		this.name = name;
+		this.price = price;
+		this.stars = stars;
+	}
 
-public Chicken()
-{
-this.name = "";
-this.price = 0.0;
-this.stars = 0;
+	public String get_name()
+	{
+		return this.name;
+	}
+
+	public double get_price()
+	{
+		return this.price;
+	}
+
+	public int get_stars()
+	{
+		return this.stars;
+	}
+
+	public void set_name(String name)
+	{
+		this.name = name;
+	}
+
+	public void set_price(double price)
+	{
+		this.price = price;
+	}
+	public void set_stars(int stars)
+	{
+		this.stars = stars;
+	}
+
 }
-
-public Chicken(String name, double price, int stars)
-{
-this.name = name;
-this.price = price;
-this.stars = stars;
-}
-
-public String get_name()
-{
-return this.name;
-}
-
-public double get_price()
-{
-return this.price;
-}
-
-public int get_stars()
-{
-return this.stars;
-}
-
-public void set_name(String name)
-{
-this.name = name;
-}
-
-public void set_price(double price)
-{
-this.price = price;
-}
-
--- src/main/java/edu/handong/csee/java/lab07/prob2/Chicken_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken_main.java
@@ -1,18 +1,25 @@
 package edu.handong.csee.java.lab07.prob2;
 
-public class Chicken_main {
-public static void main(String[] args) {
-Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5);
-Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5);
-Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);
+/**
+ * Main class for Chicken.
+ * initialize name and rating for menu1,2,3
+ * and print out the name and the rating at the end.
+ * @author Admin
+ *
+ */
+public class Chicken_main { //declare class Chicken_main
+	public static void main(String[] args) { //declare static method main which will run only once
+		Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5); // set name, price and rating for menu1
+		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5); // set name, price and rating for menu2
+		Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);// set name, price and rating for menu3
 
-menu1.set_stars(3);
-menu2.set_stars(4);
-menu3.set_stars(1);
+		menu1.set_stars(3); // call set_stars to set menu1's rating as 3
+		menu2.set_stars(4); // call set_stars to set menu2's rating as 4
+		menu3.set_stars(1); // call set_stars to set menu3's rating as 1
 
-System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars());
-System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());
-System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());
-}
+		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars()); // print out menu1's name and rating 
+		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars()); // print out menu2's name and rating
+		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars()); // print out menu3's name and rating
+	}
 
 }
